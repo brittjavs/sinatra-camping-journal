225b57f56fd4f63700bef533512fb11c736039dc
Brittany Javalera <brittjavalera@gmail.com>
2019-10-06 13:30:24 -0700

    created and ran seed file to test our code if there are errors later on

---
+++ camping-journal/db/seeds.rb
@@ -0,0 +1,5 @@
+Camper.create(email: 'billie@cutepups.com', password: 'yummytreats', username: 'BillieThePup')
+Camper.create(email: 'foxy@feistydragons.com', password: 'fightme', username: 'CallMeRambo')
+
+CampingTrip.create(date_camped: '10/07/2018', park: 'Angeles National Forest', campground: 'Table Mountain', notes: 'Most bathrooms are closed after Sept. Trails not marked well', camper_id: 1)
+CampingTrip.create(date_camped: '12/24/2017', park: 'Joshua Tree', campground: 'White Tank', notes: 'Super Windy at night into early afternoon. Lots of Coyotes', camper_id: 2)
