b5e1568944f67684096f12bde667803d61b7a910
Brittany Javalera <brittjavalera@gmail.com>
2019-10-06 15:39:16 -0700

    added logic to signup route. Created signup form which the signup route will direct to. Created post action which will validate params and create a new camper

--- camping-journal/app/controllers/campers_controller.rb
+++ camping-journal/app/controllers/campers_controller.rb
@@ -1,7 +1,21 @@
 class CampersController < ApplicationController
 
     get '/signup' do
+        if logged_in?
+            redirect to '/trips'
+        else
         erb :"campers/signup"
+        end
+    end
+
+    post '/signup' do
+        if params[:camper].empty?
+            redirect to '/signup'
+        else
+            @camper = Camper.create(params)
+            session[:user_id] = @camper.id
+            redirect to '/trips'
+        end
     end
 
     get '/login' do
