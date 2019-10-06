771849eeefbaabad06c8993cc55a265903ca8cc7
Brittany Javalera <brittjavalera@gmail.com>
2019-10-06 12:45:19 -0700

    added routes to campers controller

--- camping-journal/app/controllers/campers_controller.rb
+++ camping-journal/app/controllers/campers_controller.rb
@@ -3,4 +3,13 @@ class CampersController < ApplicationController
     get '/signup' do
         erb :"campers/signup"
     end
+
+    get '/login' do
+        erb :"campers/login"
+    end
+
+    get '/logout' do
+        erb :"campers/logout"
+    end
+    
 end
