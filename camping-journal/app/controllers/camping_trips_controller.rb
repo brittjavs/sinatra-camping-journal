6c4b332b09637351528f06fe4e055b5bdbf1831c
Brittany Javalera <brittjavalera@gmail.com>
2019-10-06 12:53:10 -0700

    renamed camping trip view files for easier understanding. Added routes to camping_trips controller. Still need to add logic to all routes

--- camping-journal/app/controllers/camping_trips_controller.rb
+++ camping-journal/app/controllers/camping_trips_controller.rb
@@ -1,2 +1,30 @@
 class CampingTripsController < ApplicationController
+    get '/trips' do
+        erb :"/camping_trips/index"
+    end
+
+    get '/trips/new' do
+        erb :"/camping_trips/create_trip"
+    end
+
+    post '/trips' do
+        redirect to '/trips'
+    end
+
+    get '/trips/:id' do
+        erb :"/camping_trips/show"
+    end
+
+    get '/trips/:id/edit' do
+        erb :"/camping_trips/edit_trip"
+    end
+
+    patch '/trips/:id' do
+        redirect to '/trips/:id'
+    end
+
+    delete '/trips/:id' do
+        redirect to '/trips/index'
+    end
+
 end
