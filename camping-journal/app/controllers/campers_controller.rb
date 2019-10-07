57c94c0da3e97b9fd1d2c74ccf8d913d949cbf71
Brittany Javalera <brittjavalera@gmail.com>
2019-10-06 18:55:21 -0700

    added logic to /trips/new route to confimr a user is logged in before routing to form. Created for in create_trip viewer which consists of form with a field for each column and submit button.

--- camping-journal/app/controllers/campers_controller.rb
+++ camping-journal/app/controllers/campers_controller.rb
@@ -9,7 +9,7 @@ class CampersController < ApplicationController
     end
 
     post '/signup' do
-        if params[:camper].empty?
+        if params[:camper].empty? #will need revision, should i check params or validate in model?
             redirect to '/signup'
         else
             @camper = Camper.create(params)
@@ -19,7 +19,21 @@ class CampersController < ApplicationController
     end
 
     get '/login' do
+        if logged_in?
+            redirect to '/trips'
+        else
         erb :"campers/login"
+        end
+    end
+
+    post '/login' do
+        @camper = Camper.find_by(params[:username])
+        if @camper && @camper.authenticate(params[:password])
+            session[:user_id] = @camper.id
+            redirect to '/trips'
+        else
+            redirect to '/login'
+        end
     end
 
     get '/logout' do
