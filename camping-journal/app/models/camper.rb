4a6ec2b359b3f32ed2a5bcffb7af946cda072eb1
Brittany Javalera <brittjavalera@gmail.com>
2019-10-13 12:43:38 -0700

    added password presence validation

--- camping-journal/app/models/camper.rb
+++ camping-journal/app/models/camper.rb
@@ -1,6 +1,6 @@
 class Camper < ActiveRecord::Base
     has_secure_password
     has_many :camping_trips
-    validates :email, :username, presence: true
+    validates :email, :username, :password, presence: true
     validates :email, :username, uniqueness: true
 end
